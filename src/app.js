75e1e6292840b827e23b24fa0db1456ec6d09299
Ethan Apodaca <papodaca@gmail.com>
2018-07-27 22:12:48 -0700

    Add dark theme.

--- src/app.js
+++ src/app.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { Provider } from "react-redux";
 
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import Container from "./components/Container";
 import configureStore from "./store/configureStore"
 
 import './app.css'
 
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
+
 const App = (props) => {
   return (
     <Provider store={configureStore(props)}>
-      <Container />
+      <MuiThemeProvider theme={theme}>
+        <Container />
+      </MuiThemeProvider>
     </Provider>
   );
 };
